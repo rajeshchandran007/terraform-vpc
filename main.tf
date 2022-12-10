d90f2ccaaabeb5a534e66b7118a21a5f77bc6d0e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-10 09:19:17 +0530

    Auto Push Commit

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 module "vpc" {
-  source            = "./vendor/modules/vpc"  
-  VPC_CIDR          = var.VPC_CIDR
-  ENV                = var.ENV
+  source              = "./vendor/modules/vpc"  
+  VPC_CIDR            = var.VPC_CIDR
+  ENV                 = var.ENV
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+  v
 
 }
 
