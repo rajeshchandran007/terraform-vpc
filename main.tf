b50082917424f05390a0d7134269e3aacfe589a6
mvpc-11232 <mvp2612@hotmail.com>
2022-12-10 08:12:45 +0530

    Auto Push Commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "vpc" {
+  source = "git::https://github.com/b51-clouddevops/tf-module-vpc.git"
+}
+
+# We cannot parameterise anything on source of the module. So that means we cannot use the variables.
+# How can I tell to my source module to fetch the code from a branch of my choice ? By default, it only refers to main branch
+# Alternatives to make it flexible ?
